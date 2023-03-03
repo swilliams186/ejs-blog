2120f88af7578fa4d67fa6ddb11a818acc1ea3cc
swilliams186 <steven.williams186@gmail.com>
2023-03-03 10:47:45 +0000

    complete project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash/string");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -24,7 +25,8 @@ app.listen(3000, function() {
 
 app.get("/", function(req, res){
   res.render("home", {
-    homeStartingContent: homeStartingContent
+    homeStartingContent: homeStartingContent,
+    posts: posts
   });
 });
 
@@ -50,6 +52,16 @@ app.post("/compose", function(req, res){
     blogTitle: req.body.blogTitle,
     blogText: req.body.blogText
   });
-  console.log(posts);
-  res.render("compose");
+
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = lodash.lowerCase(req.params.postName);
+  console.log("Requested Title lodash: " + requestedTitle);
+  posts.forEach(function(post){
+    if(lodash.lowerCase(post.blogTitle) === requestedTitle){
+      res.render("post", {post:post});
+    }
+  });
 });
